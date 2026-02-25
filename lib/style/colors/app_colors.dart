a3633f8c2b10f71758bd83b7375ab2b3d1c805c7
rachmatrisky <rachmatrisky5@gmail.com>
2026-02-26 02:08:05 +0800

    create app colors

---
+++ lib/style/colors/app_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Brand colors
+  static const primary = Color(0xFF4A90E2);
+  static const secondary = Color(0xFF50E3C2);
+
+  // Neutral
+  static const background = Color(0xFFF5F7FA);
+  static const surface = Colors.white;
+
+  // Text
+  static const textPrimary = Color(0xFF1A1A1A);
+  static const textSecondary = Color(0xFF777777);
+
+  // States
+  static const success = Color(0xFF2ECC71);
+  static const error = Color(0xFFE74C3C);
+}
