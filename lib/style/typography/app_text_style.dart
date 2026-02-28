1d77018e1eb8379302a98ebd15a99fc2c081d2b5
rachmatrisky <rachmatrisky5@gmail.com>
2026-02-28 15:43:37 +0800

    update app text style to roboto

--- lib/style/typography/app_text_style.dart
+++ lib/style/typography/app_text_style.dart
@@ -1,27 +1,108 @@
-import 'package:flutter/material.dart';
-import '../colors/app_colors.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTextStyles {
-  static const fontFamily = 'Poppins';
+  static TextStyle get _base => GoogleFonts.roboto();
 
-  static const displayLarge = TextStyle(
-    fontSize: 32,
-    fontWeight: FontWeight.bold,
-    fontFamily: fontFamily,
-    color: AppColors.textPrimary,
+  static TextStyle displayLarge = _base.copyWith(
+    fontSize: 57,
+    fontWeight: FontWeight.w700,
+    height: 1.11,
+    letterSpacing: -2,
   );
 
-  static const titleLarge = TextStyle(
-    fontSize: 20,
+  static TextStyle displayMedium = _base.copyWith(
+    fontSize: 45,
     fontWeight: FontWeight.w600,
-    fontFamily: fontFamily,
-    color: AppColors.textPrimary,
+    height: 1.17,
+    letterSpacing: -1,
   );
 
-  static const bodyMedium = TextStyle(
-    fontSize: 14,
+  static TextStyle displaySmall = _base.copyWith(
+    fontSize: 36,
+    fontWeight: FontWeight.w500,
+    height: 1.25,
+    letterSpacing: -1,
+  );
+
+  static TextStyle headlineLarge = _base.copyWith(
+    fontSize: 32,
+    fontWeight: FontWeight.w600,
+    height: 1.5,
+    letterSpacing: -1,
+  );
+
+  static TextStyle headlineMedium = _base.copyWith(
+    fontSize: 28,
+    fontWeight: FontWeight.w500,
+    height: 1.2,
+    letterSpacing: -1,
+  );
+
+  static TextStyle headlineSmall = _base.copyWith(
+    fontSize: 24,
     fontWeight: FontWeight.w400,
-    fontFamily: fontFamily,
-    color: AppColors.textSecondary,
+    height: 1.0,
+    letterSpacing: -1,
+  );
+
+  static TextStyle titleLarge = _base.copyWith(
+    fontSize: 22,
+    fontWeight: FontWeight.w500,
+    height: 1.2,
+    letterSpacing: 1.2,
+  );
+
+  static TextStyle titleMedium = _base.copyWith(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    height: 1.2,
+    letterSpacing: 1.2,
+  );
+
+  static TextStyle titleSmall = _base.copyWith(
+    fontSize: 14,
+    fontWeight: FontWeight.w300,
+    height: 1.2,
+    letterSpacing: 1.2,
+  );
+
+  static TextStyle bodyLargeBold = _base.copyWith(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    height: 1.56,
+  );
+
+  static TextStyle bodyLargeMedium = _base.copyWith(
+    fontSize: 14,
+    fontWeight: FontWeight.w300,
+    height: 1.56,
+  );
+
+  static TextStyle bodyLargeRegular = _base.copyWith(
+    fontSize: 12,
+    fontWeight: FontWeight.w200,
+    height: 1.56,
+  );
+
+  static TextStyle labelLarge = _base.copyWith(
+    fontSize: 14,
+    fontWeight: FontWeight.w300,
+    height: 1.71,
+    letterSpacing: 1.3,
+  );
+
+  static TextStyle labelMedium = _base.copyWith(
+    fontSize: 12,
+    fontWeight: FontWeight.w200,
+    height: 1.4,
+    letterSpacing: 1.3,
+  );
+
+  static TextStyle labelSmall = _base.copyWith(
+    fontSize: 11,
+    fontWeight: FontWeight.w100,
+    height: 1.2,
+    letterSpacing: 1.3,
   );
 }
