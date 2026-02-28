b56e0d79395582ddd62b4f5c592e5d2885d4b971
rachmatrisky <rachmatrisky5@gmail.com>
2026-02-28 15:42:50 +0800

    create home screen

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:gym_app/widgets/sliding_card.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(icon: Icon(Icons.people_alt), onPressed: () {}),
+        title: Text('Welcome User!'),
+        centerTitle: true,
+        actions: [IconButton(icon: Icon(Icons.doorbell), onPressed: () {})],
+      ),
+      body: SingleChildScrollView(
+        child: Column(children: [SlidingChallengeCard()]),
+      ),
+    );
+  }
+}
